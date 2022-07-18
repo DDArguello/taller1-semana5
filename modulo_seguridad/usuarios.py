56c153e89fe1478cff4711e942cf2e177413ab51
DDArguello <portagame1234@gmail.com>
2022-07-18 18:27:20 -0500

    Add files via upload

--- modulo_seguridad/usuarios.py
+++ modulo_seguridad/usuarios.py
@@ -6,33 +6,25 @@ class Usuario:
         self.telefono=telef
     
 
-# class UserAdmin(Usuario):
+class UserAdmin(Usuario):
     
-#     def __init__(self,nombre,ced,telf,correo,id_user=1):
-#        # if id_rol==1:
-#            # self.id_rol="Administrador"
-#         if id_user==1:
-#             self.id_user="Seguridad del sistema"
+    def __init__(self,nombre,ced,telf,correo):
 
-#         super().__init__(ced,nombre,telf,correo)
+        super().__init__(ced,nombre,telf,correo)
     
-    
+    def mostrar(self):
+        print(f"Usuario: {self.ced} {self.name} {self.correo} {self.telefono}")
 
-# class UserOper(Usuario):
+class UserOper(Usuario):
     
-#     def __init__(self,nombre,ced,telf,correo,id_user=2):
-#         #if id_rol==2:
-#             #self.id_rol="Operador"
-#         if id_user==2:
-#             self.id_user="Mantenimiento del sistema"
+    def __init__(self,nombre,ced,telf,correo):
     
-#         super().__init__(ced,nombre,telf,correo)
+        super().__init__(ced,nombre,telf,correo)
         
-#     def mostrar(self):
-#         print(f"Usuario: {self.ced} {self.name} {self.correo} {self.telefono} Cargo: {self.id_rol} de {self.id_user}")
+    def mostrar(self):
+        print(f"Usuario: {self.ced} {self.name} {self.correo} {self.telefono}")
 
-
-class Rol(Usuario):
+class opcSistema(Usuario):
     def __init__(self,nombre,ced,telf,correo,id_rol,id_user=0):
         if id_rol==2:
             self.id_rol="Operador"
@@ -54,5 +46,5 @@ class Rol(Usuario):
 
 if __name__ == "__main__":
 
-    rol=Rol("David","0957436675","0982337416","darguello@gmail.com",1)
-    rol.mostrar()
+    user=opcSistema("David","0957436675","0982337416","darguello@gmail.com",1)
+    user.mostrar()
